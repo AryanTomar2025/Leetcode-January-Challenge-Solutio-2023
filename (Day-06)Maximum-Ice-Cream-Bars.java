bc11eecb7513e26f04cf3e4da86b9ae94d667700
AryanTomar2025 <aryantomarcs@gmail.com>
2023-01-06 21:33:14 +0530

    Create (Day-06)Maximum-Ice-Cream-Bars.java

---
+++ (Day-06)Maximum-Ice-Cream-Bars.java
@@ -0,0 +1,14 @@
+class Solution {
+    
+    public int maxIceCream(int[] costs, int coins) {
+        
+        Arrays.sort(costs);
+        int iceCream = 0, i = 0;
+        while(i < costs.length && coins >= costs[i]){
+            coins -= costs[i];
+            iceCream++;
+            i++;
+        }
+        return iceCream;
+    }
+}
